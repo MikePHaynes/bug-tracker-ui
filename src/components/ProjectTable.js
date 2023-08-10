8d8ee2c0f852a979de9de14bdde99664f1143b9c
MikePHaynes <86630042+MikePHaynes@users.noreply.github.com>
2023-08-09 17:00:55 -0700

    Edit project table component.

--- src/components/ProjectTable.js
+++ src/components/ProjectTable.js
@@ -4,6 +4,9 @@ import axios from 'axios';
 export const ProjectTable = () => {
   const API_BASE_URL = 'http://localhost:8080/api/projects';
   const [projects, setProjects] = useState([]);
+  const [tickets, setTickets] = useState([]);
+  const [showProjects, setShowProjects] = useState(true);
+  const [showTickets, setShowTickets] = useState(false);
 
   useEffect(() => {
     fetchProjects();
@@ -18,14 +21,33 @@ export const ProjectTable = () => {
     }
   };
 
+  const fetchProjectTickets = async (project) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/${project.id}/tickets`);
+      setTickets(response.data);
+      handleViewTickets();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleViewTickets = () => {
+    setShowProjects(false);
+    setShowTickets(true);
+  };
+
   return (
     <div>
-      <table className="table-auto">
+      {showProjects && <table className="table-auto">
         <thead>
           <tr>
             <th>Name</th>
             <th>Description</th>
             <th>Created At</th>
+            <th>View</th>
+            <th>Tickets</th>
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
@@ -34,10 +56,34 @@ export const ProjectTable = () => {
               <td>{project.projectName}</td>
               <td>{project.projectDescription}</td>
               <td>{project.createdAt}</td>
+              <td><button>View</button></td>
+              <td><button onClick={() => fetchProjectTickets(project)}>Tickets</button></td>
+              <td><button>Edit</button></td>
+              <td><button>Delete</button></td>
             </tr>
           ))}
         </tbody>
-      </table>
+      </table>}
+      {showTickets && <table className="table-auto">
+      <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Severity</th>
+            <th>Created At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tickets.map(ticket => (
+            <tr key={ticket.id}>
+              <td>{ticket.ticketName}</td>
+              <td>{ticket.ticketDescription}</td>
+              <td>{ticket.severityLevel}</td>
+              <td>{ticket.createdAt}</td>
+            </tr>
+          ))}
+        </tbody>
+        </table>}
     </div>
   );
 };
