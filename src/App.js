77847cc30e9148c6dbfe79ed42c0d8521ef7e066
MikePHaynes <86630042+MikePHaynes@users.noreply.github.com>
2023-08-20 14:17:58 -0700

    Edit navbar to use bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
+import { NavigationBar } from './components/NavigationBar';
 import { HomePage } from './pages/HomePage';
 import { ProjectPage } from './pages/ProjectPage';
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <NavigationBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/projects" element={<ProjectPage />} />
